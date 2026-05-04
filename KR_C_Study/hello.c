fb5e77c5eceed41416c2c9ad141f6b48629fdb5d
CodeByeori <brpark202@gmail.com>
2026-05-04 23:34:12 +0900

    K&R 노트

---
+++ KR_C_Study/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // 표준 입출력 라이브러리의 함수를 이용
+
+int main()                                /// main 함수 정의. 매개변수는 없다
+{                                         // main 함수 시작
+    printf("hello\nhello\thello\bhello"); // 문자출력을 위해 printf 함수 호출, 매개변수는 "hello"
+    return 0;
+} // main 함수 종료
+
+//
